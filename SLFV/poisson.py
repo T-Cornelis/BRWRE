51789aab6acf55d0b17b754fa3aa39b4f29ee7c0
T-Cornelis <35939742+T-Cornelis@users.noreply.github.com>
2019-07-11 23:42:13 +0200

    poisson

--- SLFV/poisson.py
+++ SLFV/poisson.py
@@ -16,12 +16,12 @@ import time
 
 class inh_ppp:
 	def __init__(self, size, inh_function, **kwargs):
-		# The parameters indicate:
-		# Size         = sizes of the square on which we construct the process.
+		"The parameters indicate:"
+		#Size         = sizes of the square on which we construct the process.
 		#                If multidimensional, size is supposed to be a VECTOR!
 		# Ihn_function = spatial rate/intensity/inhomogeneity of the PPP.
 		self.size  = size
-		self.dim   = np.shape(size)
+		self.dim   = np.size(size)
 		self.inh_f = inh_function
 
 		# We need an upper bound for the function for our algorithm.
@@ -32,25 +32,31 @@ class inh_ppp:
 			self.max_f = 1
 
 	def generate(self):
-		# First we generate a homogeneous PPP.
+		"Generate a homogeneous PPP"
 
 		# 1) Total number of particles
-		self.total  = np.random.poisson(lam = np.prod(self.size), size = 1)
+		self.total  = np.random.poisson(lam = np.prod(self.size))
 		# 2) Conditional on total number, sample independent, uniform points
-		self.points = np.random.uniform(low = np.zeros(size = self.dim), high = self.size, size = [self.total, self.dim])
+		self.points = np.random.uniform(low = np.zeros(shape = self.dim), high = self.size, size = [self.total, self.dim])
 
 		# Then we generate the inhomogeneous one, by deleting the ones which have low probability of happening.
 
-		self.likely   = self.inh_f(self.points)/self.max_f
-		self.tocancel = 1 - np.random.binomial(n=1, p = self.likely, shape = self.total)
+		# We can do this, unless we have no points to delete.
+		if self.points.size:
+			self.likely = self.inh_f(self.points)/self.max_f
+			self.accept = np.random.binomial(n=1, p = self.likely, size = self.	total)
+			# We transform the result to Boolean values
+			self.accept = [ _==1 for _ in self.accept ]
 
-		self.points = self.points[self.tocancel, :]
+			self.points = self.points[self.accept, :]
 
 		return self.points
 
+def input_f( input ):
+	return 0.5
 
-time_horizon = 10
-box_size     = 10
+# This is to try out the code:
+# Below the signature indicates that this should map vectors to numbers
 
-# Simulate a Poisson Point Process.
-total_num_events = np.random.poisson(lam = box_size**2)
+#vinput_f = np.vectorize(input_f, signature = '(m)->()')
+#prova = inh_ppp(size = [1.0, 1.0, 1.0, 2.0], inh_function = vinput_f)
--- SLFV/slfv.py
+++ SLFV/slfv.py
@@ -12,10 +12,39 @@ from os import path
 import sys
 import time
 
-# For now we do not ad any selection.
+# To simulate a (inhomogeneous) Poisson Point Process we import our package:
+import poisson as ps
 
-time_horizon = 10
-box_size     = 10
+# 0) We define the parameters of the problem:
+DIM       = 2
+SIZE      = 10
+TIME_HORZ = 5
 
-# Simulate a Poisson Point Process.
-total_num_events = np.random.poisson(lam = box_size**2)
+# 1) We define the selection coefficient (which should always take values in (-1, 1) ):
+def selection(input):
+	return 0.2*np.sin(np.dot(input, input))
+def selection_pl(input):
+	return np.clip(0.2*np.sin(np.dot(input, input)), a_min = 0, a_max = None)
+def selection_mn(input):
+	return -np.clip(0.2*np.sin(np.dot(input, input)), a_min = None, a_max = 0)
+# which we then vectorize:
+vselection    = np.vectorize(selection, signature = '(n)->()')
+vselection_pl = np.vectorize(selection_pl, signature = '(n)->()')
+vselection_mn = np.vectorize(selection_mn, signature = '(n)->()')
+
+# 2) We define the neutral coefficient:
+def neutral(input):
+	return 1 - np.abs(selection(input))
+# which we also vectorize
+vneutral = np.vectorize(neutral, signature = '(n)->()')
+
+# 3) We define the neutral events:
+pois_neu = ps.inh_ppp(size = [SIZE, SIZE, TIME_HORZ], inh_function = vneutral)
+neutral_events = pois_neu.generate()
+
+# 4) We define the positive selective events:
+pois_sel_pl  = ps.inh_ppp(size = [SIZE, SIZE, TIME_HORZ], inh_function = vselection_pl)
+selection_pl_events = pois_sel_pl.generate()
+pois_sel_mn  = ps.inh_ppp(size = [SIZE, SIZE, TIME_HORZ], inh_function = vselection_mn)
+selection_mn_events = pois_sel_mn.generate()
+embed()
